bc626fcd262a341f11021e11e704ea8b41d9cf78
Victor Ciappina <vc@intelligentbanker.com>
2020-07-01 15:17:26 +0200

    Feat: Present sorted products at products GET request

--- backend/functions/products.js
+++ backend/functions/products.js
@@ -3,10 +3,10 @@ const { callDynamoDB } = require('./layers/share/dynamodb')
 
 module.exports.handler = async event => {
   var params = { TableName : "products" }
-  let result = await callDynamoDB('scan', params)
-
+  let products = await callDynamoDB('scan', params)
+  products = products.sort((a, b) => (a.id > b.id) ? 1 : -1)
   return {
     statusCode: 200,
-    body: JSON.stringify(result,null,2),
+    body: JSON.stringify(products,null,2),
   }
 }
